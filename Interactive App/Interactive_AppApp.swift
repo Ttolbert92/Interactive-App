39e9cedcaadc24c174149588d01472673cbebf67
Tamika Tolbert <beatty.tamika@gmail.com>
2023-04-17 18:52:10 -0700

    Initial Commit

---
+++ Interactive App/Interactive_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_AppApp.swift
+//  Interactive App
+//
+//  Created by Deangelo Tolbert on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
